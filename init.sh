d6c6e3f3e8c5870ade02fbf098340bb513e39fe5
vbawol <vbawol@veteranbastards.com>
2017-03-31 20:52:36 -0500

    revert

--- init.sh
+++ init.sh
@@ -62,7 +62,7 @@ ln -s $ARMASVRPATH"/keys"  $ARMASVRPATH"/Keys"
 #convert to lower case
 toLower() 
 {
-ls | while read upName; do loName=`echo "${upName}" | tr '[:upper:]' '[:lower:]'`; mv "$upName" "$loName" 2>/dev/null; done
+ls | while read upName; do loName=`echo "${upName}" | tr '[:upper:]' '[:lower:]'`; mv "$upName" "$loName"; done
 }
 
 # perform install of mods
